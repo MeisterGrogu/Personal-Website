212b7a3dc5425babcdb07310d7c8f5a51be3e316
Your Name <you@example.com>
2025-05-02 21:24:50 +0200

    Adding more projects

--- src/content/Projekte.js
+++ src/content/Projekte.js
@@ -1,12 +1,29 @@
-import { cpp, python, linux, csharp, unity, java, html, css, js, react, tailwindCss } from "./badges";
+import { cpp, python, linux, csharp, unity, java, html, css, js, react, tailwindCss, c, sdl } from "./badges";
 export const projekte_data = [
     {
-        name: "Portfolio",
-        imagePath: "/assets/projectSnaps/project3a.jpg",
+        name: "Portfolio Websites",
+        imagePath: "/assets/projectSnaps/portfolio.png",
         description: "Meine Portfolio Website mit dem allseits bekannten Windows 95 look.",
-        githubLink: "https://github.com/MeisterGrogu",
+        githubLink: "https://github.com/MeisterGrogu/Personal-Website",
         websiteLink: "https://leopold-peters.de/",
         techStack: [react, tailwindCss]
     },
 
+    {
+        name: "JFFL (Just For Fun Language)",
+        imagePath: "/assets/projectSnaps/JFFL.png",
+        description: "Mein Versuch eine eigene Programmiersprache zu erstellen. (Work in progress)",
+        githubLink: "https://github.com/MeisterGrogu/JFFL",
+        websiteLink: "https://leopold-peters.de/",
+        techStack: [c]
+    },
+
+    {
+        name: "Jayden Engine",
+        imagePath: "/assets/projectSnaps/Jayden.png",
+        description: "Meine eigene Game Engine. (Unfertig)",
+        githubLink: "https://github.com/MeisterGrogu/Jayden-Engine",
+        websiteLink: "https://leopold-peters.de/",
+        techStack: [cpp, sdl, c]
+    },
 ]
--- src/content/badges.js
+++ src/content/badges.js
@@ -4,13 +4,15 @@ export const js = "https://img.shields.io/badge/-JavaScript-F7DF1E?logo=javascri
 export const react = "https://img.shields.io/badge/-React%20JS-61DAFB?logo=react&logoColor=black&style=for-the-badge";
 export const nodeJs = "https://img.shields.io/badge/-Node%20JS-339933?logo=node.js&logoColor=white&style=for-the-badge";
 export const cpp = "https://img.shields.io/badge/C++-00599C?style=for-the-badge&logo=c%2B%2B&logoColor=white";
+export const c = "https://img.shields.io/badge/C-00599C?style=for-the-badge&logo=c&logoColor=white"
 export const python = "https://img.shields.io/badge/Python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54";
 export const csharp = "https://img.shields.io/badge/C%23-239120?style=for-the-badge&logo=c-sharp&logoColor=white";
 export const unity = "https://img.shields.io/badge/Unity-100000?style=for-the-badge&logo=unity&logoColor=white";
 export const java = "https://img.shields.io/badge/Java-ED8B00?style=for-the-badge&logo=java&logoColor=white";
 export const linux = "https://img.shields.io/badge/Linux-FCC624?style=for-the-badge&logo=linux&logoColor=black";
 export const tailwindCss = "https://img.shields.io/badge/-Tailwind%20css-06B6D4?logo=tailwindcss&logoColor=white&style=for-the-badge";
+export const sdl = "https://img.shields.io/badge/SDL-FF6600?style=for-the-badge&logo=sdl&logoColor=white"
 
 export const badges = [
-    cpp, python, linux, csharp, unity, java, html, css, js, react, nodeJs, tailwindCss
+    cpp, python, linux, csharp, unity, java, html, css, js, react, nodeJs, tailwindCss, sdl
 ]
